dfe36511beea28ce14d2cbd2f88a2f954154a33d
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-19 15:30:05 +0600

    first commit

---
+++ functionWithOptionalParameter.js
@@ -0,0 +1,32 @@
+
+// //--Old system--//
+// function add(a, b){
+//     return a+b;
+// }
+
+// console.log(add(5, 2));
+
+// //--Old system output NaN when you don't pass any parameter--//
+// function add(a, b){
+//     return a+b;
+// }
+
+// console.log(add(5));
+
+// //--Old system solution to solve without passing parameter value--//
+// function add(a, b){
+//     // b = b || 0;
+//     if(b == undefined){
+//         b = 0; 
+//     }
+//     return a+b;
+// }
+
+// console.log(add(5));
+
+// //--New system for call a function and passing parameter.--//
+function add(a, b = 0){
+    return a+b;
+}
+
+console.log(add(5));
