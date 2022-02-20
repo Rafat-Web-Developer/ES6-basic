d640264bf0d1367f7b3c1f388da864624723153e
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-20 23:19:41 +0600

    update inheritence.js

--- inheritance.js
+++ inheritance.js
@@ -1,30 +1,25 @@
 class Product {
     productCategory;
-    constructor(getProductCategory){
+    name;
+    price;
+    constructor(getProductCategory, getName, getPrice){
         this.productCategory = getProductCategory;
+        this.name = getName;
+        this.price = getPrice;
     }
 };
 
 class ComputerProduct extends Product{
-    name;
-    price;
-    constructor(productCategory, getName, getPrice){
-        super(productCategory);
-        this.name = getName;
-        this.price = getPrice;
-    }
     work(){
         console.log(`Price of ${this.name} is ${this.price}.`);
     };
 };
 
 class MobilePhone extends Product{
-    name;
-    price;
-    constructor(productCategory, getName, getPrice){
-        super(productCategory);
-        this.name = getName;
-        this.price = getPrice;
+    location;
+    constructor(productCategory, name, price, getLocation){
+        super(productCategory, name, price);
+        this.location = getLocation;
     }
     work(){
         console.log(`Price of ${this.name} is ${this.price}.`);
@@ -33,7 +28,7 @@ class MobilePhone extends Product{
 
 const printer = new ComputerProduct('Computer', "Printer", 12000);
 const mouse = new ComputerProduct('Computer', "Mouse", 300);
-const mobilePhone = new MobilePhone('Mobile', 'Huawei Nova 3i', 24000);
+const mobilePhone = new MobilePhone('Mobile', 'Huawei Nova 3i', 24000, 'Bangladesh');
 console.log(printer);
 console.log(mouse);
 console.log(mobilePhone);
