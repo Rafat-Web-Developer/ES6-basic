eac87cf621435f4703fd55b9c846a308c42cc2a7
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-20 17:31:01 +0600

    Add Spread Operator

---
+++ spreadOperator.js
@@ -0,0 +1,8 @@
+const friends = ['Jubair', 'Emon', 'Shohailol', 'Fahim', 'Sayem'];
+console.log(...friends);
+
+const numbers = [10, 20, 30, 50, 40];
+console.log(Math.max(50, 25, 15, 65));
+console.log(Math.max(numbers)); // Returns NaN;
+console.log(Math.max(...numbers));
+
