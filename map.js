882a3ddec6822d8540aaa3b08d3573c3ea68d65a
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-20 18:19:15 +0600

    update map.js

--- map.js
+++ map.js
@@ -1,3 +1,10 @@
+// 1. completing the loop for every element
+// 2. for each value execute the function which you doing
+// 3. Create an array for each output after execution and put it in a variable
 const numbers = [1, 2, 3, 4, 5];
 const output = numbers.map(x => x * 2);
 console.log(output);
+
+const numbers2 = [2, 3, 4];
+const output2 = numbers2.map(x => x * x);
+console.log(output2);
